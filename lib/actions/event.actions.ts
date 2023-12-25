f110cf12328ec6d478c5a635f66fad7e7626855b
Gaurav Mishra <gaurav.info217@gmail.com>
2023-12-25 21:11:41 +0530

    updated prisma schema

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import {PrismaClient} from '@prisma/client';
+import {CreateEventParams} from '@/types';
+import { handleError } from '../utils';
+
+const prisma = new PrismaClient();
+
+export const createEvent = async(eventParams:CreateEventParams) => {
+    try{
+        const {event,userId,path} = eventParams;
+        const organizer = await prisma.user.findUnique({
+            where : {
+                id : userId
+            }
+        })
+        if(!organizer){
+            throw new Error('Invalid Organizer')
+        }
+
+        const newEvent = 
+    }
+    catch(error){
+        handleError(error);
+    }
+}
